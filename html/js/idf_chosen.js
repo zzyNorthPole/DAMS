00a7dacc35ce3a6eb9c1a4fd2929ea2e7ae02901
zzynorthpole <2422588629@qq.com>
2022-11-15 05:37:24 +0800

    update

--- html/js/idf_chosen.js
+++ html/js/idf_chosen.js
@@ -8,4 +8,19 @@ function idf_chosen(obj) {
         document.getElementById("user_type_left_side").style.display = "block";
     }
 }
-get("http://47.97.18.183:8002/auth/user-type", idf_chosen);
+var xml_http_idf = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+xml_http_idf.onreadystatechange = function() {
+    if (xml_http_idf.readyState == 4) {
+        if (xml_http_idf.status == 200) {
+            var text = xml_http_idf.responseText;
+            var obj = JSON.parse(text);
+            idf_chosen(obj);
+        }
+        else if (xml_http_idf.status == 404) {
+            window.location.href = "login.html";
+        }
+    }
+}
+xml_http_idf.open("get", "http://47.97.18.183:8002/auth/user-type", true);
+xml_http_idf.withCredentials = true;
+xml_http_idf.send();
